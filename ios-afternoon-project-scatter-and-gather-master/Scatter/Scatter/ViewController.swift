b2fb360bf307a2c2ee55f641f96b286a62ce4d28
AustinPotts <pottsevolvements@gmail.com>
2019-10-08 17:18:08 -0400

    MVP Reached
    ISSUE: My random color function isnt working properly when Labels fall into animation.

--- ios-afternoon-project-scatter-and-gather-master/Scatter/Scatter/ViewController.swift
+++ ios-afternoon-project-scatter-and-gather-master/Scatter/Scatter/ViewController.swift
@@ -22,6 +22,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var logoImage: UIImageView!
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
     
     
     
@@ -31,30 +35,70 @@ class ViewController: UIViewController {
         
         UIView.animateKeyframes(withDuration: 4, delay: 0, options: [], animations: {
             
+            let color = self.randomColor()
+            
+            // L Label Animations
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
                 self.L.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
+                
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
+                self.L.center = CGPoint(x: 50, y: 200)
+                self.L.layer.backgroundColor = color
             }
             
+            
+            // A1 Label Animations
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
                 self.A1.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
+                self.A1.layer.backgroundColor = color
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
+                self.A1.center = CGPoint(x: 100, y: 20)
             }
             
+            
+            //M Label Animations
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
                 self.M.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
+                self.M.layer.backgroundColor = color
             }
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.M.center = CGPoint(x: 20, y: 150)
+            })
             
+            
+            //B Label Animations
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
                 self.B.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
             }
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.B.center = CGPoint(x: 73, y: 109)
+                self.B.layer.backgroundColor = color
+            })
             
+            
+            //D Label Animations
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
                 self.D.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
+                self.D.layer.backgroundColor = color
             }
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.D.center = CGPoint(x: 55, y: 180)
+            })
             
+            
+            //A2 Label Animations
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
                 self.A2.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
+                self.A2.layer.backgroundColor = color
             }
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                self.A2.center = CGPoint(x: 80, y: 340)
+            })
             
+            
+            //Logo Animations
             UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
                 self.logoImage.alpha = 0
             })
@@ -68,11 +112,17 @@ class ViewController: UIViewController {
     
     var isScattered: Bool?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    
+    
+    private func randomColor() -> CGColor {
+        let red = CGFloat.random(in: 0...255)
+        let green = CGFloat.random(in: 0...255)
+        let blue = CGFloat.random(in: 0...255)
+        
+        return UIColor(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1.0).cgColor
+    }
+    
 
 }
 
