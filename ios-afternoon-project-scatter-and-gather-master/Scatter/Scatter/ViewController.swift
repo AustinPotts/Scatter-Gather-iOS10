a7d8887b7ef91c5a842c6aefe95f5472ab5cd9e9
AustinPotts <pottsevolvements@gmail.com>
2019-10-08 16:32:07 -0400

    Project Configured/ Outlets SetUp

---
+++ ios-afternoon-project-scatter-and-gather-master/Scatter/Scatter/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Scatter
+//
+//  Created by Austin Potts on 10/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var L: UILabel!
+    @IBOutlet weak var A1: UILabel!
+    @IBOutlet weak var M: UILabel!
+    @IBOutlet weak var B: UILabel!
+    @IBOutlet weak var D: UILabel!
+    @IBOutlet weak var A2: UILabel!
+    
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+    }
+    
+    var isScattered: Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
